619080aaef3fd756f8fb567de8bff9b63c94f0c0
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-19 23:18:28 +0200

    create controller and store controller added for categories, category store request added, create and store routes added for categories, models upd, migrations for softdeletes added, category create view added

--- app/Http/Controllers/Admin/Category/IndexController.php
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\View\View;
 
 class IndexController extends Controller
 {
+    /**
+     * @return View
+     */
     public function __invoke(): View
     {
-        return view('admin.categories.index');
+        $categoriesList = Category::all();
+
+        return view('admin.categories.index', compact('categoriesList'));
     }
 }
