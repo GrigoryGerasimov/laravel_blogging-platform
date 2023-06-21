45a43ac52df0957c4ff61160759ebd529ae3a697
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-21 10:05:47 +0200

    posts edit view refactored, posts edit and update controllers refactored, posts update request refactored

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\{Storage, DB, Log};
 
 class UpdateController extends Controller
 {
@@ -16,7 +17,29 @@ class UpdateController extends Controller
      */
     public function __invoke(UpdateRequest $request, Post $post): RedirectResponse
     {
-        $post->update($request->validated());
+        try {
+            DB::beginTransaction();
+
+            $data = $request->validated();
+            $postTags = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            if (array_key_exists('preview_img', $data)) {
+                $data['preview_img'] = Storage::disk('public')->put('images/preview', $data['preview_img']);
+            }
+            if (array_key_exists('main_img', $data)) {
+                $data['main_img'] = Storage::disk('public')->put('images/main', $data['main_img']);
+            }
+
+            $post->update($data);
+            $post->tags()->sync($postTags);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            Log::error($e->getMessage(), $e->getTrace());
+        }
 
         return redirect()->route('admin.post.show', compact('post'));
     }
