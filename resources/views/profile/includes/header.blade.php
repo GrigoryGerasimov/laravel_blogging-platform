094e1c5625f68d12784626d23d6a1cba15fbb180
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-23 23:22:05 +0200

    switching between profile and admin in header implemented

--- resources/views/profile/includes/header.blade.php
+++ resources/views/profile/includes/header.blade.php
@@ -1,7 +1,14 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light d-flex justify-content-between">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.index') }}" role="button">
+                <i class="nav-icon fas fa-user-cog"></i>
+            </a>
         </li>
     </ul>
     <form action="{{ route('logout') }}" method="POST" enctype="application/x-www-form-urlencoded">
